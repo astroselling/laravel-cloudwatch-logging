9083b28024003e222d2ab1a3f988437b5152ee38
Gabriel Piriz <gabriel@iugo.com.uy>
2023-04-07 14:57:43 -0300

    Update format function to comply with Monolog

--- src/CloudWatchFormatter.php
+++ src/CloudWatchFormatter.php
@@ -2,22 +2,20 @@
 
 namespace Astroselling\LaravelCloudwatchLogging;
 
-use Aws\CloudWatchLogs\CloudWatchLogsClient;
-use Maxbanton\Cwh\Handler\CloudWatch;
 use Monolog\Formatter\LineFormatter;
-use Monolog\Logger;
+use Monolog\LogRecord;
 
 class CloudWatchFormatter extends LineFormatter
 {
     /**
      * {@inheritDoc}
      */
-    public function format(array $record): string
+    public function format(LogRecord $record): string
     {
         $record['context']['log_message'] = $record['message'];
         $record['context']['extra'] = $record['extra'];
-        $record['message'] = null;
         $record['extra'] = [];
+
         return parent::format($record);
     }
 }
